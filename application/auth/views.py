d442342441edb40b9878d501ea5dd52268c21b6e
Jeeses313 <46317768+Jeeses313@users.noreply.github.com>
2019-04-02 19:21:27 +0300

    Fixing heroku

--- application/auth/views.py
+++ application/auth/views.py
@@ -34,8 +34,7 @@ def auth_form():
 
 @app.route("/auth/<user_id>/")
 def auth_profile(user_id):
-    data=user_id
-    res = db.engine.execute("SELECT User.id, User.username, COUNT(Review.id) AS review_count FROM User LEFT JOIN Review ON User.id = Review.user_id WHERE User.id= %s GROUP BY User.id", data)
+    res = db.engine.execute("SELECT User.id, User.username, COUNT(Review.id) FROM User LEFT JOIN Review ON User.id = Review.user_id WHERE User.id= ? GROUP BY User.id", user_id)
     users = []
     for row in res:
         users.append({"id":row[1], "username":row[1], "review_count":row[2]})
